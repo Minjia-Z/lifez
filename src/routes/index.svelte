962ad4682aeccf8e9362a320753085547b3bc99a
Minjia Zhu <minjia.bianca.zhu@gmail.com>
2022-04-23 18:57:33 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script>
+	let links = [""];
+</script>
+
+<div class="bg-white p-10 font-main min-h-screen">
+	<div class="flex flex-wrap-reverse justify-between gap-10 items-center">
+		<div class="flex flex-col gap-8 justify-center max-w-md flex-1">
+			<h1 class="text-5xl font-bold">Birthday Coins</h1>
+			<p>
+				What does time mean to you? We are defined by the dates that are
+				valuable to us. This is how we perceive time because important
+				things happen on these time marks that are like pearls on a
+				string. Ever since the day we’re born, we look forward to
+				birthdays because great memories are attached to these dates,
+				and we hope to make them even better for you and the people you
+				care about.
+			</p>
+			<p>
+				The LifeZ project will involve a number of small NFT projects,
+				all centered around our mission of making lives better. LifeZ
+				will also be actively donating to charities that fund brain
+				disease research and/or taking care of people in need. We want
+				to make our first NFT project - Birthdayz Coinz accessible,
+				hence the starting price point of 1 SOL.
+			</p>
+			<p>
+				This will grant ownership to a beautiful, rare, and
+				well-detailed 3D coin. Owners will also have the opportunity to
+				participate in our process of making changes and improve other
+				people’s lives.
+			</p>
+			<div class="flex gap-4">
+				<button class="btn btn-primary rounded-full"
+					>Download App</button
+				>
+				<button class="btn btn-primary rounded-full">Watch Video</button
+				>
+			</div>
+		</div>
+
+		<img class="flex-1 object-cover" src="/ballons_nbg.png" alt="baloons" />
+	</div>
+
+	<button>Arrow Down</button>
+</div>
---
+++ src/routes/test.svelte
@@ -0,0 +1,82 @@
+<script>
+    // var video = document.getElementById("LandingVideo");
+    // var btn = document.getElementById("myBtn");
+
+    // function myFunction() {
+    //     if (video.paused) {
+    //         video.play();
+    //         btn.innerHTML = "Pause";
+    //     } else {
+    //         video.pause();
+    //         btn.innerHTML = "Play";
+    //     }
+    // }
+</script>
+
+<video autoplay muted loop id="LandingVideo">
+    <source src="src/party.mp4" type="video/mp4" />
+</video>
+
+<div class="content">
+    <h1>Welcome to the Birthday Club!</h1>
+    <p>
+        Enter a 4 digit pin to enter the secret club.<br />
+        (hint: your birthday in mmdd format)
+    </p>
+    <form>
+        <label for="birthday">Secret entry pin:</label><br />
+        <input type="number" id="birthday" name="birthday" /><br /><br /><br />
+    </form>
+    <button id="myBtn">Enter</button>
+</div>
+
+<style>
+    /*@import url('https://rsms.me/inter/inter.css');*/
+    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap");
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 0;
+        font-family: Arial;
+        font-size: 17px;
+    }
+
+    #LandingVideo {
+        position: absolute;
+        z-index: -1;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .content {
+        font-family: "Inter", sans-serif;
+        position: absolute;
+        top: 35%;
+        left: 25%;
+        background: rgba(0, 0, 0, 0.6);
+        color: #f1f1f1;
+        width: 50%;
+        padding: 40px;
+        text-align: center;
+        border-radius: 10px;
+    }
+
+    #myBtn {
+        width: 200px;
+        font-size: 18px;
+        padding: 10px;
+        border: none;
+        background: #000;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    #myBtn:hover {
+        background: #ddd;
+        color: black;
+    }
+</style>
